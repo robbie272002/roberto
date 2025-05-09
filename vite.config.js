507183fced32ad3844db2a1160dcd34945dd6f9f
robbie272002 <robertojrvelasco359@gmail.com>
2025-05-09 11:36:24 +0800

    Include robertovelasco directory as part of main repo

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactAppSpa/',
+  base: '/roberto/',
   server: {
-    port: 3001,
+    port: 3000,
     open: true,
- 
+    cors: true,
   }
 })
