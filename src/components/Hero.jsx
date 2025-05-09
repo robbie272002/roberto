70c34ff1b923e270304272e588fee54ea7f5eefc
robbie272002 <robertojrvelasco359@gmail.com>
2025-05-09 12:06:56 +0800

    resolveconflict

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             <h1 class="fs-2 fs-lg-1 text-white fw-bold mb-2 mb-lg-x1 lh-base mt-3 mt-lg-0">Manage your remote <span class="text-nowrap">team work</span></h1>
             <p class="fs-8 text-white mb-3 mb-lg-4 lh-lg">With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes</p>
             <div class="d-flex justify-content-center justify-content-lg-start"><a class="btn btn-primary btn-lg lh-xl mb-4 mb-md-5 mb-lg-7" href="#!">Explore more</a></div>
-            <p class="mb-x1 fs-10 button-text text-uppercase fw-bold lh-base text-300">Download our app</p>
+            <p class="mb-x1 fs-10 button-text text-uppercase fw-bold lh-base text-300">Download our app now</p>
             <div class="d-flex flex-wrap justify-content-center justify-content-lg-start gap-2 position-relative z-2"><a class="border-0 p-0 bg-transparent cursor-pointer rounded-1" href="#!">
                 <img class="img-fluid" src={appstore} alt="assets/img/logos/App_Store.webp" /></a><a class="border-0 p-0 bg-transparent cursor-pointer rounded-1" href="#!"> 
                 <img class="img-fluid" src={playstore} alt="assets/img/logos/Play_Store.webp" /></a></div>
